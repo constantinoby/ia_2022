f98efff2e91cb84cefb999beba9226472c6ec304
Constantino Byelov <csbyelov@gmail.com>
2022-11-17 10:27:24 +0100

    A + H

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -4,11 +4,12 @@ sys.path.append('C:\\Users\\csbye\\OneDrive\\Escritorio\\IA\\Github\\ia_2022')
 
 from practica1 import agent_amplada,agent_heuristica,agent_minimax, joc
 
-
+"""Heuristica + Amplada done"""
 def main():
     #rana = agent_amplada.Rana("Miquel")
     rana = agent_heuristica.Rana("Miquel")
     lab = joc.Laberint([rana], parets=True)
+
 
     #rana = agent_minimax.Rana("Papafritta", es_max=True)
     #rana2 = agent_minimax.Rana("Billy", es_max=False)
